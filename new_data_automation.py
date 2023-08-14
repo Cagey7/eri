7de1b58532e24e4c8cf06dd553eb8b137afe8f7a
Cagey7 <dagodecagey@gmail.com>
2023-08-14 22:38:20 +0600

    Imporved code

--- new_data_automation.py
+++ new_data_automation.py
@@ -1,6 +1,7 @@
-from datetime import datetime 
+from datetime import datetime, timedelta 
 import psycopg2
 import requests
+from regions import regions
 
 
 class Automation:
@@ -12,8 +13,27 @@ class Automation:
         self.password = password
         self.conn = None
         self.cur = None
+        self.db_connection()
+        table_regions = """
+        CREATE TABLE IF NOT EXISTS regions (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(200)
+        );
+        """
+        self.cur.execute(table_regions)
+        self.cur.execute("SELECT 1 FROM regions LIMIT 1;")
+        table_is_full = self.cur.fetchone()
+
+        if not table_is_full:
+            for element in regions:
+                insert_query = """
+                INSERT INTO regions (name)
+                VALUES (%s);
+                """
+                self.cur.execute(insert_query, (element,))
     
-    def dbconnection(self):
+
+    def db_connection(self):
         if self.conn is None:
             try:
                 self.conn = psycopg2.connect(
@@ -33,83 +53,58 @@ class Automation:
 
     def insert_gdp(self):
         url = 'https://taldau.stat.gov.kz/ru/Api/GetIndexData/2709379?period=7&dics=67' 
-        response = requests.post(url)
+        response = requests.get(url)
         if response.status_code == 200:
-            table_gdp_regions = """
-            CREATE TABLE IF NOT EXISTS gdp_regions (
-                id SERIAL PRIMARY KEY,
-                name VARCHAR(200)
-            );
-            """
-            table_gdp = """
-            CREATE TABLE IF NOT EXISTS gdp (
-                id SERIAL PRIMARY KEY,
-                created_at DATE,
-                value BIGINT,
-                updated_at DATE,
-                region_id INT REFERENCES gdp_regions(id)
-            );
-            """
+            try:
+                table_gdp = """
+                CREATE TABLE IF NOT EXISTS gdp (
+                    id SERIAL PRIMARY KEY,
+                    created_at DATE,
+                    value BIGINT,
+                    updated_at DATE,
+                    region_id INT REFERENCES regions(id)
+                );
+                """
 
-            self.cur.execute(table_gdp_regions)
-            self.cur.execute(table_gdp)
-
-            self.cur.execute("SELECT MAX(updated_at) FROM gdp;")
-            latest_date = self.cur.fetchone()[0]
-            if latest_date == None:
-                latest_date = datetime.strptime("1.1.1970", "%d.%m.%Y").date()
-
-
-            data = response.json()
-            kz_data = []
-            filter_data = []
-            regions = set()
-
-            for row in data:
-                kz_data.append(row)
-
-            for row in kz_data:
-                regions.add(row["termNames"][0])
-            
-            for row in kz_data:
-                unit_data = []
-                for period in row["periods"]:
-                    date_object = datetime.strptime(period["date"], "%d.%m.%Y").date()
-                    if date_object > latest_date:
-                        unit_data = [period["date"], period["value"], row["termNames"][0]]
-                        filter_data.append(unit_data)
-            
-
-            self.cur.execute("SELECT 1 FROM gdp_regions LIMIT 1;")
-            row_exists = self.cur.fetchone()
-
-            if not row_exists:
-                for element in regions:
-                    insert_query = """
-                    INSERT INTO gdp_regions (name)
-                    VALUES (%s);
-                    """
-                    self.cur.execute(insert_query, (element,))
-
-            for row in filter_data:
-                date = row[0]
-                value = row[1]
-                name = row[2]
                 insert_query = """
                 INSERT INTO gdp (created_at, value, updated_at, region_id)
-                VALUES (TO_DATE(%s, 'DD.MM.YYYY'), %s, CURRENT_DATE, (SELECT id FROM gdp_regions WHERE name = %s));
+                VALUES (TO_DATE(%s, 'DD.MM.YYYY'), %s, CURRENT_DATE, (SELECT id FROM regions WHERE name = %s));
                 """
-                self.cur.execute(insert_query, (date, value, name))
-        
-            self.conn.commit()
-            print("Данные о ВВП загружены.")
+
+                self.cur.execute(table_gdp)
+
+                self.cur.execute("SELECT MAX(updated_at) FROM gdp;")
+                latest_date = self.cur.fetchone()[0]
+                latest_date = latest_date - timedelta(days=1) if latest_date else datetime.strptime("1.1.1970", "%d.%m.%Y").date()
+
+
+                data = [row for row in response.json()]
+                filtered_data = []
+
+                for row in data:
+                    unit_data = []
+                    for period in row["periods"]:
+                        date_object = datetime.strptime(period["date"], "%d.%m.%Y").date()
+                        if date_object > latest_date:
+                            unit_data = [period["date"], period["value"], row["termNames"][0]]
+                            filtered_data.append(unit_data)
+
+                insert_data = [(row[0], row[1], row[2]) for row in filtered_data]
+
+                self.cur.executemany(insert_query, insert_data)
+                self.conn.commit()
+                
+                print("Данные о ВВП загружены.")
+            except Exception as e:
+                self.conn.rollback()
+                print("Произошла ошибка:", str(e))
         else:
             print(f"Ошибка запроса: {response.status_code}")
             print("Новая попытка")
             self.insert_gdp()
 
 
-    def dbdisconnect(self):
+    def db_disconnect(self):
         if self.conn is not None:
             self.conn.close()
             self.conn = None
@@ -137,6 +132,5 @@ class Automation:
 
 
 automation = Automation("statistics", "postgres", "123456")
-automation.dbconnection()
 automation.collect_data_years()
-automation.dbdisconnect()
+automation.db_disconnect()
